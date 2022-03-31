20c5f75bb81e0e27206051d802dc322d2a7c28da
kennethleanda <leandakenneth@gmail.com>
2022-03-31 18:58:42 +0800

    navbar hover

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -8,25 +8,65 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto justify-content-center">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Men</a>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Men
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <div class = "row row-no-gutters">
+            <div class = col-sm-12>
+              <p> READY TO WEAR</p> 
+                  <a class="dropdown-item" href="#">Tees</a>
+                  <a class="dropdown-item" href="#">Pants</a>
+                  <a class="dropdown-item" href="#">Shorts</a>
+            </div>
+            <div class = col-sm-12>
+              <p>SHOES</p> 
+                  <a class="dropdown-item" href="#">Sneakers</a>
+            </div>
+          </div>
+        </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Women</a>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Women
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
       </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         Accesories
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Accesories</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Others</a>
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Others
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
       </li>
     </ul>
 
     <div class="search-box">
-      <i class="bi bi-search"></i>
-      <input class="search-txt" type="text" name="" placeholder="Search"> 
-      <a class = "search-btn" href=""> </a>
+       
+            <input class="form-control mr-sm-1" type="text" name="" placeholder="Search" > 
+       
     </div>
   </div>
 </nav>
