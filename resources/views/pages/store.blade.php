d93e3ddb74156620404cfbb72d26bd9e1076d6d3
kennethleanda <leandakenneth@gmail.com>
2022-04-09 08:56:49 +0800

    sidebar

--- resources/views/pages/store.blade.php
+++ resources/views/pages/store.blade.php
@@ -1,7 +1,15 @@
-@extends('layout.app')
+@extends('layout.store')
+
 @section('userbar')
+
 <div class="d-flex justify-content-end bg-light border-bottom border-dark">
     <a class="text-decoration-none text-dark me-2" href="/login"><i class="bi bi-key pe-1"></i>Login</a>
     <a class="text-decoration-none text-dark pe-3" href="/register"><i class="bi bi-pen pe-1"></i>Register</a>
 </div>
+
 @endsection
+
+
+
+
+
