eb9916fd733ed27c3a556c013500c50efaf30eb2
Dghost901 <carlocarreon66@gmail.com>
2022-04-21 16:31:54 +0800

    comment

---
+++ resources/views/pages/historypage.blade.php
@@ -0,0 +1,30 @@
+@extends('layout.app')
+@include('inc.userbar')
+
+@section('content')
+
+<div class="bidding">
+        <div class="bidding-container">
+            <div class="row">
+                <div class="col">
+                    <div class="title">
+                        <h3><b>BIDDING HISTORY</b></h3>
+                        
+                    </div>
+                </div>
+            </div>  
+    </div>
+        <div class="col">
+                            <div class="details">
+                                <h3><b>Recent Biddings</b></h3> 
+                                    <div class="item-det">
+                                        <h5>Product Name: </h5>
+                                        <h5>Size: M</h5>
+                                        <h5>Price: 999.99</h5>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+        
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Route::get('/store','App\Http\Controllers\PagesController@store');
 Route::resource('/register','App\Http\Controllers\UsersController');
 Route::get('/login','App\Http\Controllers\PagesController@login');
 Route::post('/login','App\Http\Controllers\LoginsController@login');
-
 Route::get('/productpage','App\Http\Controllers\PagesController@productpage');
+Route::get('/historypage','App\Http\Controllers\PagesController@historypage');
 
 //admin module
 Route::get('/admin/index','App\Http\Controllers\PagesController@admin_index');
