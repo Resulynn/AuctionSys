702153282e57dff0c0a5cdbf20569fe837d3c999
Dghost901 <carlocarreon66@gmail.com>
2022-04-21 16:40:14 +0800

    comment

--- resources/views/pages/historypage.blade.php
+++ resources/views/pages/historypage.blade.php
@@ -10,20 +10,28 @@
                     <div class="title">
                         <h3><b>BIDDING HISTORY</b></h3>
                         
-                    </div>
-                </div>
-            </div>  
-    </div>
-        <div class="col">
+                        <div class="col">
                             <div class="details">
-                                <h3><b>Recent Biddings</b></h3> 
+                            <br></br>
+                                <h3>Recent Biddings</h3> 
                                     <div class="item-det">
-                                        <h5>Product Name: </h5>
+                                        <h5>Product Name:</h5>
                                         <h5>Size: M</h5>
                                         <h5>Price: 999.99</h5>
-                                        
                                     </div>
-                                </div>
+                                    <br></br>
+                                    <div class="item-det">
+                                        <h5>Product Name:</h5>
+                                        <h5>Size: M</h5>
+                                        <h5>Price: 999.99</h5>
+                                    </div>
+                                    <br></br>
+                                    <div class="item-det">
+                                        <h5>Product Name:</h5>
+                                        <h5>Size: M</h5>
+                                        <h5>Price: 999.99</h5>
+                                    </div>
+                                
                             </div>
                         </div>
         
