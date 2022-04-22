6cee174735a73bc3bcc7a937063abb57e7b9b93b
kennethleanda <leandakenneth@gmail.com>
2022-04-22 22:45:44 +0800

    admin frontend and navbar update

--- resources/views/inc/userloggedbar.blade.php
+++ resources/views/inc/userloggedbar.blade.php
@@ -1,6 +1,17 @@
 <div class="userloggedbar d-flex justify-content-end bg-white py-2 px-4">
     <div class="userloggedbar-content">
-        <a class="py-1" href="/logout"><i class="bi bi-box-arrow-left px-2"></i>LOGOUT, {{Session::get('username')}}</a>
+       {!! Form::open(['action'=>'App\Http\Controllers\LogoutController@logout',
+        'method'=>'POST']) !!}
+        <i class="bi bi-box-arrow-left px-2"></i>
+        <a href="">{{Form::('LOGOUT', ['class="py-1 pe-1"'])}}
+        {{Session::get('username')}}
+        </a> 
+        {{
+
+            
+        }}
+        {!! Form::close() !!}
+        
     </div>
 </div>
 
